648d762c08274e97ac7f45fb1284c569ba9bacc3
Cássio Laurentino <cassiosalinilaurentino@hotmail.com>
2022-12-14 09:07:59 -0300

    initial commit

---
+++ src/test/java/com/cassiolaurentino/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassiolaurentino.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
